baca254e5113d20cd48e068834ebac3c25b6b067
Michael Overmeyer <m.overmeyer@yahoo.ca>
2015-06-01 00:27:54 +0000

    Added quick flag to save 5 seconds.

--- install_everything.sh
+++ install_everything.sh
@@ -80,4 +80,4 @@ openssl req -new -x509 -sha256 -days 365 -nodes -subj "/C=CA/ST=Ontario/L=Waterl
 #Start the Mediawiki
 docker run -d -p 80:80 -p 443:443 -v ${MW_DIR}:/src/mediawiki -v ${IMAGES_DIR}:/src/mediawiki/images -v ${SSL_CERT_DIR}:/etc/ssl/localcerts --link mysql:mysql -e MYSQL_PASSWORD=`cat ${MYSQL_PASS_FILE}` --name=${DOCKER_IMAGE} ${DOCKER_IMAGE}
 
-docker exec -i -t ${DOCKER_IMAGE} bash -c "cd /src/mediawiki/maintenance && php5 update.php"
+docker exec -i -t ${DOCKER_IMAGE} bash -c "cd /src/mediawiki/maintenance && php5 update.php --quick"
